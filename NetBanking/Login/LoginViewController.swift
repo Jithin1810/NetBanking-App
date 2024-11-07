4b8ce2a92555836f8c7b1f15618482a565c25c4b
JiTHiN <jithin.181099@gmail.com>
2024-11-07 17:56:24 +0530

    Adding LoginScreen

---
+++ NetBanking/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetBanking
+//
+//  Created by JiTHiN on 07/11/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NetBanking/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  NetBanking
+//
+//  Created by JiTHiN on 07/11/24.
+//
+
+import Foundation
